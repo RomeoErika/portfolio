65d93d7b50079c0c1596fa80fe84b3eb729d126c
Daroms <romeoerikarazakandriarimanana@gmail.com>
2021-11-08 17:37:48 +0100

    my first commit 8-11-2021/18:32

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+window.onload = () =>{
+	const navButton = document.querySelector(".navigation-menu svg");
+	const navigation = document.querySelector(".navigation-right");
+	navButton.addEventListener("click",function () {
+		navigation.classList.toggle("show");
+		this.src.toggle(".https://mattfarley.ca/")
+	})
+
+
+
+
+
+
+
+
+}
