4157b9a536208937bcc637e20b4b6f91c9ffbc14
Daroms <romeoerikarazakandriarimanana@gmail.com>
2021-11-10 11:04:57 +0100

    ajout de la slide

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,44 @@
 window.onload = () =>{
 	const navButton = document.querySelector(".navigation-menu svg");
 	const navigation = document.querySelector(".navigation-right");
+	var slideIndex = 1;
+	const rightButton = document.querySelector(".right svg");
+	const leftButton = document.querySelector(".left svg" );
+	var dots = document.querySelectorAll(".dot");
 	navButton.addEventListener("click",function () {
 		navigation.classList.toggle("show");
-		this.src.toggle(".https://mattfarley.ca/")
-	})
+	});
+	rightButton.addEventListener("click",function() {
+		Nextimage(1)
+	});
+	leftButton.addEventListener("click",function() {
+		Nextimage(-1)
+	});
+
+	showSlides(slideIndex);
+	function Nextimage(n) {
+		showSlides(slideIndex += n);
+	}
+	function imageActuel(n) {
+		showSlides(slideIndex = n);
+	}
+	function showSlides(n) {
+  			var i;
+  			var slides = document.getElementsByClassName("image-content");
+
+  			if (n > slides.length) {
+  				slideIndex = 1
+  			}
+  			if (n < 1) {
+  				slideIndex = slides.length
+  			}
+  			for (i = 0; i < slides.length; i++) {
+     			 slides[i].style.display = "none";
+  			}
+  			 for (i = 0; i < dots.length; i++) {
+      				dots[i].className = dots[i].className.replace(" active", "");
+  			}
+ 	 		slides[slideIndex-1].style.display = "block";
+ 	 		dots[slideIndex-1].className += " active";
+	} 
 }
